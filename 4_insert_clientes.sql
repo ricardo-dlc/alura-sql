48ab018fe0b08b5b4fd5a081bf9d566c59e902b1
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-11-05 15:57:08 -0500

    feat: Insert clientes

---
+++ 4_insert_clientes.sql
@@ -0,0 +1,47 @@
+INSERT INTO
+    tb_cliente
+VALUES (
+        9283760794,
+        "Edson Calisaya",
+        "Sta Úrsula Xitla",
+        "Barrio del Niño Jesús",
+        "Ciudad de México",
+        "EM",
+        "22002002",
+        "1995-01-07",
+        25,
+        "M",
+        150000,
+        250000,
+        1
+    ),
+    (
+        7771579779,
+        "Marcelo Perez",
+        "F.C. de Cuernavaca 13",
+        "Carola",
+        "Ciudad de México",
+        "EM",
+        "88202912",
+        "1992-01-25",
+        29,
+        "M",
+        70000,
+        160000,
+        1
+    ),
+    (
+        5576228758,
+        "Patricia Olivera",
+        "Pachuca 75",
+        "Condesa",
+        "Ciudad de México",
+        "EM",
+        "88192029",
+        "1995-01-14",
+        25,
+        "F",
+        70000,
+        160000,
+        1
+    )
