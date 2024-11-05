3e607eaa4f6b9666498413677dc78d5f187e09d3
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-11-05 15:24:30 -0500

    feat: Add products

---
+++ 3_insert_productos.sql
@@ -0,0 +1,62 @@
+INSERT INTO
+    tb_producto (
+        `CODIGO`,
+        `DESCRIPCION`,
+        `SABOR`,
+        `TAMANO`,
+        `ENVASE`,
+        `PRECIO_LISTA`
+    )
+VALUES (
+        '1040107',
+        'Light',
+        'Sandía',
+        '350 ml',
+        'Lata',
+        4.56
+    )
+
+INSERT INTO
+    tb_producto (
+        `CODIGO`,
+        `DESCRIPCION`,
+        `SABOR`,
+        `TAMANO`,
+        `ENVASE`,
+        `PRECIO_LISTA`
+    )
+VALUES (
+        '1040108',
+        'Light',
+        'Guanábana',
+        '350 ml',
+        'Lata',
+        4
+    )
+
+INSERT INTO
+    tb_producto
+VALUES (
+        '1040109',
+        'Light',
+        'Asaí',
+        '350 ml',
+        'Lata',
+        5.60
+    ),
+    (
+        '1040110',
+        'Light',
+        'Manzana',
+        '350 ml',
+        'Lata',
+        6.00
+    ),
+    (
+        '1040111',
+        'Light',
+        'Mango',
+        '350 ml',
+        'Lata',
+        3.50
+    )
